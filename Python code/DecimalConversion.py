c6148ee0a4adf4b13804e278661b3854ae492c8b
Tresa Maria Jacob <68148535+tresa2002@users.noreply.github.com>
2021-10-26 21:13:23 +0530

    Create DecimalConversion.py

---
+++ Python code/DecimalConversion.py
@@ -0,0 +1,7 @@
+# Python program to convert decimal into other number systems
+decim = 344
+
+print("The decimal value of", decim, "is:")
+print(bin(decim), "in binary.")
+print(oct(decim), "in octal.")
+print(hex(decim), "in hexadecimal.")
